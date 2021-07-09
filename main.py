ecfa73ba2d6585b19b2f5606848033ef907f1310
AYMEN DEV <68467119+steamdz@users.noreply.github.com>
2021-07-09 13:30:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import pywhatkit
+#pip install pywhatkit
+
+pywhatkit.text_to_handwriting("Hi im Aymen Zemmouri , Im 24 Years, From Algeria : Batna"
+                              "See u soon",rgb=(0,0,0))
+#b3d madir run ryh tjik photo win save lfichier project t3 python
